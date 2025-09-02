cf0f945c15ebb629712afcda92be826ec31ce918
Muhammad Ghufran <ghufranbakrie@gmail.com>
2025-09-03 00:38:35 +0700

    fix: update REST server port and cleanup

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,17 +1,15 @@
-
 import server from "$server/instance";
 import { serve } from "@hono/node-server";
-import * as Graceful from "$pkg/graceful"
+import * as Graceful from "$pkg/graceful";
 
 export function startRestApp() {
-  const app = server.restServer()
+  const app = server.restServer();
   const restServer = serve({
     fetch: app.fetch,
-    port: Number(process.env.NODE_LOCAL_PORT) || 3150,
-  })
+    port: Number(process.env.NODE_LOCAL_PORT) || 3155,
+  });
 
   Graceful.registerProcessForShutdown("rest-server", () => {
-    restServer.close()
-  })
+    restServer.close();
+  });
 }
-
